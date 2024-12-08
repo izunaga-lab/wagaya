507953721f9be6ec3ed38f51032183d6e38bf4d6
AZUKI\kazuk <kazuki20029140@gmail.com>
2024-12-08 15:03:11 +0900

    storybookの設定を微修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,8 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['./introduction.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
