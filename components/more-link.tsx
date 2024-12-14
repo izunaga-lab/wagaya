1690a2850eb9bb519ebf0a46a7e4958aaf1015e7
AZUKI\kazuk <kazuki20029140@gmail.com>
2024-12-14 12:35:59 +0900

    MoreLinkのstorybookを追加

--- components/more-link.tsx
+++ components/more-link.tsx
@@ -7,7 +7,7 @@ interface MoreLinkProps {
 export default function MoreLink({ path }: MoreLinkProps) {
   return (
     <div className="text-right mt-6">
-      <Link href={path} className="text-cyan-700 underline">
+      <Link href={path} className="text-cyan-700 underline hover:opacity-70" id={`more-link-${path.replace('/', '')}`}>
         もっとみる
       </Link>
     </div>
