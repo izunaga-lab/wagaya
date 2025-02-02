4be8c5c4bc521a3d714584716b3a8ec370cb9aa5
AZUKI\kazuk <kazuki20029140@gmail.com>
2025-02-02 20:26:54 +0900

    containerをlayoutにあげる

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -4,7 +4,9 @@ export default function NewsLayout({ children }: { children: React.ReactNode }) 
   return (
     <div>
       <PageHeadingArea title="お知らせ" iconName="info" />
-      {children}
+      <div className="container mx-auto px-4 py-12">
+        <div className="space-y-6">{children}</div>
+      </div>
     </div>
   )
 }
